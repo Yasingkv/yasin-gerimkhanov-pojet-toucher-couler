b1becfc66eb322e55ecd52b5e59d8683fdc9478d
Gerimkhanov Yasin <200460@CCM-Asty-Moulin.local>
2023-03-30 16:02:24 +0200

    toucher couler

---
+++ projet toucher couler/projet toucher couler/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace projet_toucher_couler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int ligne = 10;
+            int collone = 10;
+
+            string[,] matrix = new string[ligne, collone];
+
+            Random alea = new Random();
+            for (int i = 0; i < ligne; i++)
+            {
+                for (int j = 0; j < collone; j++)
+                {
+                    matrix[i, j] = alea.Next(2) == 0 ? "O" : "X"; //(condition) ? (valeur si vrai) : (valeur si faux)
+                }
+            }
+            for (int i = 0; i < ligne; i++)
+            {
+                for (int j = 0; j < collone; j++)
+                {
+                    Console.Write("+---");
+                }
+                Console.Write("+\n");
+
+                for (int j = 0; j < collone; j++)
+                {
+                    Console.Write("| " + matrix[i, j] + " ");
+                }
+                Console.Write("|\n");
+            }
+
+            for (int j = 0; j < collone; j++)
+            {
+                Console.Write("+---");
+            }
+            Console.Write("+\n");
+
+            grille_vide(ligne, collone);
+
+        }
+        static void grille_vide(int ligne , int collone)
+        {
+            for (int i = 0; i < ligne; i++)
+            {
+                for (int j = 0; j < collone; j++)
+                {
+                    Console.Write("+---");
+                }
+                Console.Write("+\n");
+
+                for (int j = 0; j < collone; j++)
+                {
+                    Console.Write("|   ");
+                }
+                Console.Write("|\n");
+            }
+
+            for (int j = 0; j < collone; j++)
+            {
+                Console.Write("+---");
+            }
+            Console.Write("+\n");
+
+        }
+
+    }
+}
